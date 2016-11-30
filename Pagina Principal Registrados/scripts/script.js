9d6026a6e7f7d320f852091dc4cea7b8a537a8f6
nopesal <nssalomone@gmail.com>
2016-12-01 00:06:32 +0100

    [Página principal registrados] Implementado click en el boton me gusta

--- Pagina Principal Registrados/scripts/script.js
+++ Pagina Principal Registrados/scripts/script.js
@@ -16,6 +16,7 @@ for (var i = 0; i < imagenes.length; i++) {
     imagenes[i].addEventListener("mouseover", mostrarDatosImagen, false);
     imagenes[i].addEventListener("mouseout", ocultarDatosImagen, false);
 }
+
 function mostrarDatosImagen() {
     $(this.children[1]).stop().fadeTo(300, 1);
     $(this.children[2]).stop().fadeTo(300, 1);
@@ -26,4 +27,27 @@ function ocultarDatosImagen() {
     $(this.children[1]).stop().fadeTo(300, 0);
     $(this.children[2]).stop().fadeTo(300, 0);
     $(this.children[3]).stop().fadeTo(300, 0);
+}
+
+var corazones = document.getElementsByClassName("fa-heart-o");
+for (var i = 0; i < corazones.length; i++) {
+    corazones[i].addEventListener("click", darLike, false);
+}
+
+function darLike() {
+    var corazon = $(this);
+    var contadorDeLikes = $(this).next();
+    if (corazon.hasClass("fa-heart-o")) {
+        corazon.toggleClass('fa-heart-o fa-heart');
+        var numeroDeLikes = contadorDeLikes.html();
+        numeroDeLikes = parseInt(numeroDeLikes);
+        numeroDeLikes = numeroDeLikes + 1
+        contadorDeLikes.text(numeroDeLikes);
+    } else if (corazon.hasClass("fa-heart")) {
+        corazon.toggleClass('fa-heart-o fa-heart');
+        var numeroDeLikes = contadorDeLikes.html();
+        numeroDeLikes = parseInt(numeroDeLikes);
+        numeroDeLikes = numeroDeLikes - 1;
+        contadorDeLikes.text(numeroDeLikes);
+    }
 }
