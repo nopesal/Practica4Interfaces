df5c432dacb8e7b440de221fc6b45fe29a94905b
Daniel <daniel.sarmiento.r@gmail.com>
2016-12-08 15:53:45 +0100

    [Página Principal No Registrados] Añadida función de validación a formulario de registro

--- Pagina Principal No Registrados/scripts/homepage_index_script.js
+++ Pagina Principal No Registrados/scripts/homepage_index_script.js
@@ -19,3 +19,63 @@ $(document).ready(function() {
         $("#reg-big").css("color", "#EBEBEB");
     })
 });
+
+var validReg = false;
+function validateAll() {
+    var elementoNombre = document.getElementById('cname');
+    if (elementoNombre.value == "") {
+        elementoNombre.style.backgroundColor = "#ff6666";
+        validReg = false;
+    }
+
+    var elementoApellidos = document.getElementById('uname');
+    if (elementoApellidos.value == "") {
+        elementoApellidos.style.backgroundColor = "#ff6666";
+        validReg = false;
+    }
+    var elementoEmail = document.getElementById('mail');
+    if (elementoEmail.value == "" || mailMatch() == false) {
+        elementoEmail.style.backgroundColor = "#ff6666";
+        document.getElementById('cmail').style.backgroundColor = "#ff6666";
+        validReg = false;
+    }
+    var elementoPassword = document.getElementById('psw');
+    if (elementoPassword.value == "" || passMatch() == false) {
+        elementoPassword.style.backgroundColor = "#ff6666";
+        document.getElementById('cpsw').style.backgroundColor = "#ff6666";
+        validReg = false;
+    }
+    if (validReg == true) {
+        alert("Operación realizada con éxito");
+    }
+    return validReg;
+}
+function passMatch() {
+    var password1 = document.getElementById('psw').value;
+    var password2 = document.getElementById("cpsw");
+    var passValid = true;
+    if (password1 == "" && password2.value == "") {
+        password2.style.backgroundColor = "transparent";
+    } else if (password1 != password2.value) {
+        password2.style.backgroundColor = "#ff6666";
+        passValid = false;
+    } else {
+        password2.style.backgroundColor = "#6ce695";
+    }
+    return passValid;
+}
+
+function mailMatch() {
+    var mail1 = document.getElementById("mail").value;
+    var mail2 = document.getElementById("cmail");
+    var mailValid = true;
+    if (mail1 == "" && mail2.value == "") {
+        mail2.style.backgroundColor = "transparent";
+    } else if (mail1 != mail2.value) {
+        mail2.style.backgroundColor = "#ff6666";
+        mailValid = false;
+    } else {
+        mail2.style.backgroundColor = "#6ce695";
+    }
+    return mailValid;
+}
