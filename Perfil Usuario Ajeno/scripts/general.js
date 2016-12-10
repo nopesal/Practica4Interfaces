55925a94791d9e8ca89f2d3752b38b6f12952006
Daniel <daniel.sarmiento.r@gmail.com>
2016-12-10 22:45:40 +0100

    [Perfil Usuario Ajeno] Solucionado bug en script de notificaciones. - La flecha permanecía visible al abrir menú de usuario y desaparecer el de notificaciones - Ahora desaparece de manera sincronizada

--- Perfil Usuario Ajeno/scripts/general.js
+++ Perfil Usuario Ajeno/scripts/general.js
@@ -1,16 +1,14 @@
-
-
-function seguir(){
+function seguir() {
     console.log(document.getElementById("seguir").textContent);
-    var ejecucion=0;
-    if(document.getElementById("seguir").textContent=="Seguir") {
+    var ejecucion = 0;
+    if (document.getElementById("seguir").textContent == "Seguir") {
         document.getElementById("cuadroSeguidores").childNodes[3].innerHTML = "225<br>seguidores";
         document.getElementById("seguir").className += " seguido";
         document.getElementById("seguir").innerHTML = "Siguiendo";
-        ejecucion=1;
+        ejecucion = 1;
     }
 
-    if(document.getElementById("seguir").textContent=="Siguiendo" && ejecucion==0) {
+    if (document.getElementById("seguir").textContent == "Siguiendo" && ejecucion == 0) {
         document.getElementById("cuadroSeguidores").childNodes[3].innerHTML = "224<br>seguidores";
         document.getElementById("seguir").innerHTML = "Seguir";
         document.getElementById("seguir").className = " boton";
@@ -21,23 +19,35 @@ function seguir(){
 function myMenu() {
     document.getElementById("myMenu").classList.toggle("showMenu");
     document.getElementById("myNotifications").classList.remove('showNot');
-}
-function myNotifications() {
-    document.getElementById("myNotifications").classList.toggle("showNot");
-    document.getElementById("myMenu").classList.remove('showMenu');
-
-    var delay=400; //0.4 segundos
+    var delay = 400; //0.4 segundos
 
     /*
      * Si el menú está abierto se añade un retraso para sincronizar la desaparición de la fleche
      * con la desaparición del menú.
      */
 
-    if(document.getElementById("menuNotificaciones").classList.contains('notificationsMenu')){
-        setTimeout(function() {
+    if (document.getElementById("menuNotificaciones").classList.contains('notificationsMenu')) {
+        setTimeout(function () {
             document.getElementById("menuNotificaciones").classList.toggle('notificationsMenu');
         }, delay);
-    }else{
+    }
+}
+function myNotifications() {
+    document.getElementById("myNotifications").classList.toggle("showNot");
+    document.getElementById("myMenu").classList.remove('showMenu');
+
+    var delay = 400; //0.4 segundos
+
+    /*
+     * Si el menú está abierto se añade un retraso para sincronizar la desaparición de la fleche
+     * con la desaparición del menú.
+     */
+
+    if (document.getElementById("menuNotificaciones").classList.contains('notificationsMenu')) {
+        setTimeout(function () {
+            document.getElementById("menuNotificaciones").classList.toggle('notificationsMenu');
+        }, delay);
+    } else {
         document.getElementById("menuNotificaciones").classList.toggle('notificationsMenu');
     }
 
