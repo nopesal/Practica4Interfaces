abcf1f408f8c9f115a3d2516bbbf6ed7e809e146
luismdlp <luismdlp@hotmail.com>
2016-12-11 16:56:19 +0100

    Usuario presupuesto actualizado. Scripts comentados

--- PRO/scripts/scriptPresupuestos.js
+++ PRO/scripts/scriptPresupuestos.js
@@ -2,13 +2,17 @@ $("#botonPRO").click(function(){
   location.href="pro.html";
 });
 
+//Si se hace click en el div de la solicitud de presupuesto se despliega una versión extendida
 $(".presupuesto").click(function(){
     $(".presupuesto").css("display", "none");
     $(".presupuestoExpandido").css("display", "flex");
 });
 
 
-
+/*
+ stopPropagation para que en los elementos que se puede hacer click dentro de la solictud de presupuestos se pueda
+ seguir haciendo
+ */
 $(".divUsuario .usuario a").click(function(event){
   event.stopPropagation();
 });
